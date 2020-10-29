6c0383586a0a13813efa855f7b4e31bc1b36398f
suiran256 <suiran256@gmail.com>
2020-10-30 08:52:05 +0900

    アルファベット化

--- routes/index.js
+++ routes/index.js
@@ -6,21 +6,23 @@ const moment = require('moment-timezone');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  const title = '予定調整くん';
+  const title = 'title';
   if (req.user) {
     Schedule.findAll({
       where: {
-        createdBy: req.user.id
+        createdBy: req.user.id,
       },
-      order: [['"updatedAt"', 'DESC']]
+      order: [['"updatedAt"', 'DESC']],
     }).then((schedules) => {
       schedules.forEach((schedule) => {
-        schedule.formattedUpdatedAt = moment(schedule.updatedAt).tz('Asia/Tokyo').format('YYYY/MM/DD HH:mm');
+        schedule.formattedUpdatedAt = moment(schedule.updatedAt)
+          .tz('Asia/Tokyo')
+          .format('YYYY/MM/DD HH:mm');
       });
       res.render('index', {
         title: title,
         user: req.user,
-        schedules: schedules
+        schedules: schedules,
       });
     });
   } else {
