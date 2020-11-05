20c85c304fa86e1f4803ee0c41769a300563cf80
suiran256 <suiran256@gmail.com>
2020-11-05 14:07:53 +0900

    アルファベット化

--- app/entry.js
+++ app/entry.js
@@ -12,17 +12,19 @@ $('.availability-toggle-button').each((i, e) => {
     const candidateId = button.data('candidate-id');
     const availability = parseInt(button.data('availability'));
     const nextAvailability = (availability + 1) % 3;
-    $.post(`/schedules/${scheduleId}/users/${userId}/candidates/${candidateId}`,
+    $.post(
+      `/schedules/${scheduleId}/users/${userId}/candidates/${candidateId}`,
       { availability: nextAvailability },
       (data) => {
         button.data('availability', data.availability);
-        const availabilityLabels = ['欠', '？', '出'];
+        const availabilityLabels = ['ketsu', '?', 'de'];
         button.text(availabilityLabels[data.availability]);
 
         const buttonStyles = ['btn-danger', 'btn-secondary', 'btn-success'];
         button.removeClass('btn-danger btn-secondary btn-success');
         button.addClass(buttonStyles[data.availability]);
-      });
+      }
+    );
   });
 });
 
@@ -30,12 +32,14 @@ const buttonSelfComment = $('#self-comment-button');
 buttonSelfComment.click(() => {
   const scheduleId = buttonSelfComment.data('schedule-id');
   const userId = buttonSelfComment.data('user-id');
-  const comment = prompt('コメントを255文字以内で入力してください。');
+  const comment = prompt('inputComment');
   if (comment) {
-    $.post(`/schedules/${scheduleId}/users/${userId}/comments`,
+    $.post(
+      `/schedules/${scheduleId}/users/${userId}/comments`,
       { comment: comment },
       (data) => {
         $('#self-comment').text(data.comment);
-      });
+      }
+    );
   }
 });
