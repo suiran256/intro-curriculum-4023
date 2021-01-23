8047a7d8d1537e1c6ca9269570f7b4e95ef217d8
suiran256 <suiran256@gmail.com>
2021-01-24 01:52:39 +0900

    test6微修正

--- test/test6.js
+++ test/test6.js
@@ -59,10 +59,10 @@ function SObj({
   fnAfter = fnAfterDefault,
   fnAfterEach = fnAfterEachDefault,
 } = {}) {
-  this.fnBefore = fnBefore;
-  this.fnAfter = fnAfter;
-  this.fnAfterEach = fnAfterEach;
   this.scheduleIdForDeleteArray = [];
+  SObj.prototype.fnBefore = fnBefore.bind(this);
+  SObj.prototype.fnAfter = fnAfter.bind(this);
+  SObj.prototype.fnAfterEach = fnAfterEach.bind(this);
 }
 
 const createScheduleAsync = async () => {
@@ -92,29 +92,25 @@ const createScheduleAsync = async () => {
   return { scheduleId };
 };
 
-const updateAvailabilityAsync = ({ scheduleId }) => {
-  return (async () => {
-    let res = null;
-    let resObj = null;
-    const candidate = await Candidate.findOne({
-      where: { scheduleId: scheduleId },
-    });
-    resObj = request(app)
-      .post(
-        `/schedules/${scheduleId}/users/${0}/candidates/${
-          candidate.candidateId
-        }`
-      )
-      .send({ availability: 2 });
-    res = await promisifyResEnd(resObj);
-    assert.match(res.text, /{"status":"OK","availability":2}/);
-    const availabilities = await Availability.findAll({
-      where: { scheduleId: scheduleId },
-    });
-    assert.strictEqual(availabilities.length, 1);
-    assert.strictEqual(availabilities[0].availability, 2);
-    return { scheduleId };
-  })();
+const updateAvailabilityAsync = async ({ scheduleId }) => {
+  let res = null;
+  let resObj = null;
+  const candidate = await Candidate.findOne({
+    where: { scheduleId: scheduleId },
+  });
+  resObj = request(app)
+    .post(
+      `/schedules/${scheduleId}/users/${0}/candidates/${candidate.candidateId}`
+    )
+    .send({ availability: 2 });
+  res = await promisifyResEnd(resObj);
+  assert.match(res.text, /{"status":"OK","availability":2}/);
+  const availabilities = await Availability.findAll({
+    where: { scheduleId: scheduleId },
+  });
+  assert.strictEqual(availabilities.length, 1);
+  assert.strictEqual(availabilities[0].availability, 2);
+  return { scheduleId };
 };
 
 const updateCommentAsync = ({ scheduleId }) => {
@@ -135,87 +131,83 @@ const updateCommentAsync = ({ scheduleId }) => {
   })();
 };
 
-const editScheduleAsync = ({ scheduleId }) => {
-  return (async () => {
-    let res = null;
-    let resObj = null;
-    resObj = request(app).get(`/schedules/${scheduleId}/edit`);
-    res = await promisifyResEnd(resObj);
-    const csrf = res.text.match(/name="_csrf" value="(.*?)"/)[1];
-    const setCookie = res.headers['set-cookie'];
-    resObj = request(app)
-      .post(`/schedules/${scheduleId}?edit=1`)
-      .set('cookie', setCookie)
-      .send({
-        scheduleName: 'scheduleName1kai',
-        memo: 'memo1kai',
-        candidates: 'can2\ncan3',
-        _csrf: csrf,
-      });
-    res = await promisifyResEnd(resObj);
-    const schedulePath = res.headers.location;
-    assert.strictEqual(schedulePath, `/schedules/${scheduleId}`);
-    assert.strictEqual(res.status, 302);
-    resObj = request(app).get(schedulePath);
-    res = await promisifyResEnd(resObj);
-    assert.match(res.text, /testuser/);
-    assert.match(res.text, /scheduleName1kai/);
-    assert.match(res.text, /memo1kai/);
-    assert.match(res.text, /can1/);
-    assert.match(res.text, /can2/);
-    assert.match(res.text, /can3/);
-    assert.strictEqual(res.status, 200);
-    return { scheduleId };
-  })();
+const editScheduleAsync = async ({ scheduleId }) => {
+  let res = null;
+  let resObj = null;
+  resObj = request(app).get(`/schedules/${scheduleId}/edit`);
+  res = await promisifyResEnd(resObj);
+  const csrf = res.text.match(/name="_csrf" value="(.*?)"/)[1];
+  const setCookie = res.headers['set-cookie'];
+  resObj = request(app)
+    .post(`/schedules/${scheduleId}?edit=1`)
+    .set('cookie', setCookie)
+    .send({
+      scheduleName: 'scheduleName1kai',
+      memo: 'memo1kai',
+      candidates: 'can2\ncan3',
+      _csrf: csrf,
+    });
+  res = await promisifyResEnd(resObj);
+  const schedulePath = res.headers.location;
+  assert.strictEqual(schedulePath, `/schedules/${scheduleId}`);
+  assert.strictEqual(res.status, 302);
+  resObj = request(app).get(schedulePath);
+  res = await promisifyResEnd(resObj);
+  assert.match(res.text, /testuser/);
+  assert.match(res.text, /scheduleName1kai/);
+  assert.match(res.text, /memo1kai/);
+  assert.match(res.text, /can1/);
+  assert.match(res.text, /can2/);
+  assert.match(res.text, /can3/);
+  assert.strictEqual(res.status, 200);
+  return { scheduleId };
 };
 
-const deleteScheduleAsync = ({ scheduleId }) => {
-  return (async () => {
-    let res = null;
-    let resObj = null;
-    resObj = request(app).get(`/schedules/${scheduleId}/edit`);
-    res = await promisifyResEnd(resObj);
-    const csrf = res.text.match(/name="_csrf" value="(.*?)"/)[1];
-    const setCookie = res.headers['set-cookie'];
-    resObj = request(app)
-      .post(`/schedules/${scheduleId}?delete=1`)
-      .set('cookie', setCookie)
-      .send({
-        _csrf: csrf,
-      });
-    res = await promisifyResEnd(resObj);
-    const p1 = Availability.findAll({
-      where: { scheduleId: scheduleId },
-    }).then((availabilities) => {
-      assert.strictEqual(availabilities.length, 0);
-      return;
+const deleteScheduleAsync = async ({ scheduleId }) => {
+  let res = null;
+  let resObj = null;
+  resObj = request(app).get(`/schedules/${scheduleId}/edit`);
+  res = await promisifyResEnd(resObj);
+  const csrf = res.text.match(/name="_csrf" value="(.*?)"/)[1];
+  const setCookie = res.headers['set-cookie'];
+  resObj = request(app)
+    .post(`/schedules/${scheduleId}?delete=1`)
+    .set('cookie', setCookie)
+    .send({
+      _csrf: csrf,
     });
-    const p2 = Candidate.findAll({ where: { scheduleId: scheduleId } }).then(
-      (candidates) => {
-        assert.strictEqual(candidates.length, 0);
-        return;
-      }
-    );
-    const p3 = Comment.findAll({ where: { scheduleId: scheduleId } }).then(
-      (comments) => {
-        assert.strictEqual(comments.length, 0);
-        return;
-      }
-    );
-    const p4 = Schedule.findByPk(scheduleId).then((schedule) => {
-      assert.strictEqual(!!schedule, false);
-      return;
-    });
-    await Promise.all([p1, p2, p3, p4]);
+  res = await promisifyResEnd(resObj);
+  const p1 = Availability.findAll({
+    where: { scheduleId: scheduleId },
+  }).then((availabilities) => {
+    assert.strictEqual(availabilities.length, 0);
     return;
-  })();
+  });
+  const p2 = Candidate.findAll({ where: { scheduleId: scheduleId } }).then(
+    (candidates) => {
+      assert.strictEqual(candidates.length, 0);
+      return;
+    }
+  );
+  const p3 = Comment.findAll({ where: { scheduleId: scheduleId } }).then(
+    (comments) => {
+      assert.strictEqual(comments.length, 0);
+      return;
+    }
+  );
+  const p4 = Schedule.findByPk(scheduleId).then((schedule) => {
+    assert.strictEqual(!!schedule, false);
+    return;
+  });
+  await Promise.all([p1, p2, p3, p4]);
+  return;
 };
 
 describe('/schedules', () => {
   const sObj = new SObj();
-  before(sObj.fnBefore.bind(sObj));
-  after(sObj.fnAfter.bind(sObj));
-  afterEach(sObj.fnAfterEach.bind(sObj));
+  before(sObj.fnBefore);
+  after(sObj.fnAfter);
+  afterEach(sObj.fnAfterEach);
 
   it('createSchedule', (done) => {
     createScheduleAsync()
@@ -251,9 +243,9 @@ describe('/schedules', () => {
 
 describe('/schedules/:scheduleId?edit=1', () => {
   const sObj = new SObj();
-  before(sObj.fnBefore.bind(sObj));
-  after(sObj.fnAfter.bind(sObj));
-  afterEach(sObj.fnAfterEach.bind(sObj));
+  before(sObj.fnBefore);
+  after(sObj.fnAfter);
+  afterEach(sObj.fnAfterEach);
 
   it('editSchedule', (done) => {
     createScheduleAsync()
@@ -268,9 +260,9 @@ describe('/schedules/:scheduleId?edit=1', () => {
 
 describe('/schedules/:scheduleId?delete=1', () => {
   const sObj = new SObj();
-  before(sObj.fnBefore.bind(sObj));
-  after(sObj.fnAfter.bind(sObj));
-  afterEach(sObj.fnAfterEach.bind(sObj));
+  before(sObj.fnBefore);
+  after(sObj.fnAfter);
+  afterEach(sObj.fnAfterEach);
 
   it('deleteSchedule', (done) => {
     createScheduleAsync()
