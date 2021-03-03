c7a8f18a73eac97b15d9e310a99e2f0ddd12a0de
suiran256 <suiran256@gmail.com>
2021-03-03 13:11:16 +0900

    sessionSecretを.envへ移動

--- app.js
+++ app.js
@@ -1,124 +1,82 @@
+/* eslint-disable no-unused-vars */
+'use strict';
 require('dotenv').config();
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
+const User = require('./models/index').User;
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var helmet = require('helmet');
-var session = require('express-session');
-var passport = require('passport');
-var app = express();
-
-const nodemailer = require('nodemailer');
-const transporter = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
-  secure: process.env.MAIL_SECURE,
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
-  },
-});
-const mailData = {
-  from: 'suiran256@gmail.com',
-  to: 'suiran256@gmail.com',
-  text: 'createTet\ntest2',
-  subject: 'test',
-};
-
-var indexRouter = require('./routes/index');
-var loginRouter = require('./routes/login');
-var logoutRouter = require('./routes/logout');
-var schedulesRouter = require('./routes/schedules');
-var availabilitiesRouter = require('./routes/availabilities');
-var commentsRouter = require('./routes/comments');
-
-//const db = require('./models/index');
-const db = require('./models');
-const User = db.User;
-
+const app = express();
+app.use(logger('dev'));
 app.use(helmet());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-var GitHubStrategy = require('passport-github2').Strategy;
-var GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
-var GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
-passport.serializeUser(function (user, done) {
-  done(null, user);
-});
-passport.deserializeUser(function (obj, done) {
-  done(null, obj);
-});
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
+const GitHubStrategy = require('passport-github2').Strategy;
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((obj, done) => done(null, obj));
 passport.use(
   new GitHubStrategy(
     {
       clientID: GITHUB_CLIENT_ID,
       clientSecret: GITHUB_CLIENT_SECRET,
-      callbackURL: process.env.HEROKU_URL
-        ? process.env.HEROKU_URL + 'auth/github/callback'
-        : 'http://localhost:3000/auth/github/callback',
+      callbackURL: 'http://localhost:3000/auth/github/callback',
     },
-    function (accessToken, refreshToken, profile, done) {
+    (accessToken, refreshToken, profile, done) => {
       process.nextTick(() => {
         User.upsert({
           userId: profile.id,
           username: profile.username,
-        })
-          .then(() => {
-            done(null, profile);
-          })
-          .catch(done);
+        }).then(() => done(null, profile));
       });
     }
   )
 );
 
-app.use(
-  session({
-    secret: 'e55be81b307c1c09',
-    resave: false,
-    saveUninitialized: false,
-  })
-);
 app.use(passport.initialize());
 app.use(passport.session());
 
+const indexRouter = require('./routes/index');
+const loginRouter = require('./routes/login');
+const logoutRouter = require('./routes/logout');
+const schedulesRouter = require('./routes/schedules');
+const availabilitiesRouter = require('./routes/availabilities');
+const commentsRouter = require('./routes/comments');
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
 app.use('/logout', logoutRouter);
 app.use('/schedules', schedulesRouter);
 app.use('/schedules', availabilitiesRouter);
 app.use('/schedules', commentsRouter);
-
 app.get(
   '/auth/github',
   passport.authenticate('github', { scope: ['user:email'] }),
-  function () {}
+  () => {}
 );
-
 app.get(
   '/auth/github/callback',
   passport.authenticate('github', { failureRedirect: '/login' }),
-  function (req, res) {
-    // transporter.sendMail(mailData, (error, info) => {
-    //   if (error) {
-    //     console.log(error); // エラー情報
-    //   } else {
-    //     console.log(info); // 送信したメールの情報
-    //   }
-    // });
-
-    var loginFrom = req.cookies.loginFrom;
-    // オープンリダイレクタ脆弱性対策
+  (req, res) => {
+    const loginFrom = req.cookies.loginFrom;
     if (
       loginFrom &&
       !loginFrom.includes('http://') &&
@@ -132,16 +90,13 @@ app.get(
   }
 );
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
-/* eslint-disable-next-line no-unused-vars */
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  res.locals.err = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
   res.render('error');
 });
