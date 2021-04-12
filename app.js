ca776556bd3074a42e2a5c9d522ee7929cb4e644
suiran256 <suiran256@gmail.com>
2021-04-12 12:13:35 +0900

    router/schedules error リファイン

--- app.js
+++ app.js
@@ -1,124 +1,74 @@
+/* eslint-disable no-unused-vars */
+'use strict';
 require('dotenv').config();
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
+const { User } = require('./models/index');
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var helmet = require('helmet');
-var session = require('express-session');
-var passport = require('passport');
-var app = express();
+const GithubStrategy = require('passport-github2').Strategy;
+const SESSION_SECRET = process.env.SESSION_SECRET;
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+const GITHUB_CALLBACK = process.env.GITHUB_CALLBACK;
 
-const nodemailer = require('nodemailer');
-const transporter = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
-  secure: process.env.MAIL_SECURE,
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
-  },
-});
-const mailData = {
-  from: 'suiran256@gmail.com',
-  to: 'suiran256@gmail.com',
-  text: 'createTet\ntest2',
-  subject: 'test',
-};
-
-var indexRouter = require('./routes/index');
-var loginRouter = require('./routes/login');
-var logoutRouter = require('./routes/logout');
-var schedulesRouter = require('./routes/schedules');
-var availabilitiesRouter = require('./routes/availabilities');
-var commentsRouter = require('./routes/comments');
-
-//const db = require('./models/index');
-const db = require('./models');
-const User = db.User;
-
-app.use(helmet());
+const app = express();
+app.use(logger('dev'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
-app.use(logger('dev'));
+app.use(helmet());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-var GitHubStrategy = require('passport-github2').Strategy;
-var GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
-var GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
-passport.serializeUser(function (user, done) {
-  done(null, user);
-});
-passport.deserializeUser(function (obj, done) {
-  done(null, obj);
-});
-
-passport.use(
-  new GitHubStrategy(
-    {
-      clientID: GITHUB_CLIENT_ID,
-      clientSecret: GITHUB_CLIENT_SECRET,
-      callbackURL: process.env.HEROKU_URL
-        ? process.env.HEROKU_URL + 'auth/github/callback'
-        : 'http://localhost:3000/auth/github/callback',
-    },
-    function (accessToken, refreshToken, profile, done) {
-      process.nextTick(() => {
-        User.upsert({
-          userId: profile.id,
-          username: profile.username,
-        })
-          .then(() => {
-            done(null, profile);
-          })
-          .catch(done);
-      });
-    }
-  )
-);
-
 app.use(
   session({
-    secret: 'e55be81b307c1c09',
+    secret: SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
   })
 );
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((obj, done) => done(null, obj));
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+      callbackURL: GITHUB_CALLBACK,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.upsert({ userId: profile.id, username: profile.username }).then(() =>
+        done(null, profile)
+      );
+    }
+  )
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', indexRouter);
-app.use('/login', loginRouter);
-app.use('/logout', logoutRouter);
-app.use('/schedules', schedulesRouter);
-app.use('/schedules', availabilitiesRouter);
-app.use('/schedules', commentsRouter);
-
+app.use('/', require('./routes/index'));
+app.use('/login', require('./routes/login'));
+app.use('/logout', require('./routes/logout'));
+app.use('/schedules', require('./routes/schedules'));
+app.use('/schedules', require('./routes/availabilities'));
+app.use('/schedules', require('./routes/comments'));
 app.get(
   '/auth/github',
   passport.authenticate('github', { scope: ['user:email'] }),
-  function () {}
+  () => {}
 );
-
 app.get(
   '/auth/github/callback',
   passport.authenticate('github', { failureRedirect: '/login' }),
-  function (req, res) {
-    // transporter.sendMail(mailData, (error, info) => {
-    //   if (error) {
-    //     console.log(error); // エラー情報
-    //   } else {
-    //     console.log(info); // 送信したメールの情報
-    //   }
-    // });
-
-    var loginFrom = req.cookies.loginFrom;
-    // オープンリダイレクタ脆弱性対策
+  (req, res) => {
+    const loginFrom = req.cookies.loginFrom;
     if (
       loginFrom &&
       !loginFrom.includes('http://') &&
@@ -132,21 +82,12 @@ app.get(
   }
 );
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-/* eslint-disable-next-line no-unused-vars */
-app.use(function (err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+app.use((req, res, next) => next(createError(404, 'notFound')));
+app.use((err, req, res, next) => {
+  const message = err.message;
+  const error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
-
-  console.log(err);
-
-  res.render('error');
+  res.render('error', { message, error });
 });
 
 module.exports = app;
--- routes/schedules.js
+++ routes/schedules.js
@@ -1,8 +1,10 @@
 //* eslint-disable no-unused-vars */
 'use strict';
+
 const authenticationEnsurer = require('./authentication-ensurer');
 const csrfProtection = require('csurf')({ cookie: true });
 const express = require('express');
+
 const createError = require('http-errors');
 const router = express.Router();
 const uuid = require('uuid');
@@ -15,25 +17,29 @@ const {
   Comment,
 } = require('../models/index');
 
-const fetchCreateCandidates = async (req, scheduleId) => {
-  const candidateNamesStr = req.body.candidates || '';
-  const candidates = candidateNamesStr
-    .split('\n')
-    .map((s) => s.trim().slice(0, 255))
-    .filter((s) => s !== '')
-    .map((name) => ({ candidateName: name, scheduleId }));
-  return await Candidate.bulkCreate(candidates);
-};
+function createCandidates(req, scheduleId) {
+  return (async () => {
+    const candidateNamesStr = req.body.candidates || '';
+    const candidates = candidateNamesStr
+      .split('\n')
+      .map((s) => s.trim().slice(0, 255))
+      .filter((s) => s !== '')
+      .map((name) => ({ candidateName: name, scheduleId }));
+    return await Candidate.bulkCreate(candidates);
+  })();
+}
 
-const isUpdatable = (schedule, req) =>
-  schedule && schedule.createdBy === Number(req.user.id);
+function isUpdatable(schedule, req) {
+  return schedule && schedule.createdBy === Number(req.user.id);
+}
 
-const deleteScheduleAggregate = (scheduleId) =>
-  (async () => {
-    const fetchDeleteAtModel = (Model) =>
-      Model.findAll({ where: { scheduleId } })
-        .then((elements) => elements.map((elem) => elem.destroy()))
-        .then((promises) => Promise.all(promises));
+function deleteScheduleAggregate(scheduleId) {
+  return (async () => {
+    const fetchDeleteAtModel = async (Model) => {
+      const elements = await Model.findAll({ where: { scheduleId } });
+      const promises = elements.map((elem) => elem.destroy());
+      return await Promise.all(promises);
+    };
     const fetchDeleteAtSchedule = () =>
       Schedule.findByPk(scheduleId).then((s) => s.destroy());
     await Promise.all([
@@ -44,38 +50,38 @@ const deleteScheduleAggregate = (scheduleId) =>
     ]);
     await fetchDeleteAtSchedule();
   })();
+}
 
 router.get('/new', authenticationEnsurer, csrfProtection, (req, res) => {
   res.render('new', { user: req.user, csrfToken: req.csrfToken() });
 });
 
 router.post('/', authenticationEnsurer, csrfProtection, (req, res, next) => {
-  const scheduleId = uuid.v4();
-  const updatedAt = new Date();
-  Schedule.create({
-    scheduleId: scheduleId,
-    scheduleName: req.body.scheduleName.slice(0, 255) || 'noName',
-    memo: req.body.memo.slice(0, 255) || 'noMemo',
-    createdBy: req.user.id,
-    updatedAt: updatedAt,
-  })
-    .then(() => {
-      return fetchCreateCandidates(req, scheduleId).then(() => {
-        res.redirect(`/schedules/${scheduleId}`);
-      });
-    })
-    .catch(next);
+  return (async () => {
+    const scheduleId = uuid.v4();
+    const updatedAt = new Date();
+    await Schedule.create({
+      scheduleId,
+      scheduleName: req.body.scheduleName.slice(0, 255) || 'noName',
+      memo: req.body.memo.slice(0, 255) || 'noMemo',
+      createdBy: req.user.id,
+      updatedAt,
+    });
+    await createCandidates(req, scheduleId);
+    res.redirect(`/schedules/${scheduleId}`);
+  })().catch(next);
 });
 
-router.get('/:scheduleId', authenticationEnsurer, (req, res, next) => {
+router.get('/:scheduleId', authenticationEnsurer, (req, res, next) =>
   (async () => {
     const scheduleId = req.params.scheduleId;
     const userIdMe = Number(req.user.id);
     const userMe = { userId: userIdMe, username: req.user.username };
+
     const schedule = await Schedule.findByPk(scheduleId, {
       include: { model: User, attributes: ['userId', 'username'] },
     });
-    if (!scheduleId) throw createError(404, 'notFound');
+    if (!schedule) throw createError(404, 'notFound');
 
     const fetchCandidates = () =>
       Candidate.findAll({
@@ -88,16 +94,13 @@ router.get('/:scheduleId', authenticationEnsurer, (req, res, next) => {
       const availabilities = await Availability.findAll({
         include: { model: User, attributes: ['userId', 'username'] },
         where: { scheduleId },
-        order: [
-          [User, '"username"', 'ASC'],
-          ['"candidateId"', 'ASC'],
-        ],
+        order: [[User, '"username"', 'ASC']],
       });
 
       availabilities.forEach((a) => {
-        const userId = a.User.userId;
+        const userId = a.userId;
         const candidateId = a.candidateId;
-        const map = availabilityMapMap.get(candidateId) || new Map();
+        const map = availabilityMapMap.get(userId) || new Map();
         map.set(candidateId, a.availability);
         availabilityMapMap.set(userId, map);
 
@@ -107,11 +110,14 @@ router.get('/:scheduleId', authenticationEnsurer, (req, res, next) => {
       return { availabilityMapMap, userOtherMap: userMapWork };
     };
     const fetchCommentMap = async () => {
-      const comments = await Comment.findAll({
-        where: { scheduleId: scheduleId },
+      const commentMap = new Map();
+      const comments = await Comment.findAll({ where: { scheduleId } });
+      comments.forEach((c) => {
+        commentMap.set(c.userId, c.comment);
       });
-      return new Map(comments.map((c) => [c.userId, c.comment]));
+      return commentMap;
     };
+
     const [
       candidates,
       { availabilityMapMap, userOtherMap },
@@ -123,15 +129,15 @@ router.get('/:scheduleId', authenticationEnsurer, (req, res, next) => {
     ]);
     res.render('schedule', {
       user: req.user,
-      schedule: schedule,
-      userMe,
+      schedule,
       candidates,
       availabilityMapMap,
-      userMap: userOtherMap,
+      userMe,
+      userOtherMap,
       commentMap,
     });
-  })().catch(next);
-});
+  })().catch(next)
+);
 
 router.get(
   '/:scheduleId/edit',
@@ -162,7 +168,7 @@ router.post(
   '/:scheduleId',
   authenticationEnsurer,
   csrfProtection,
-  (req, res, next) => {
+  (req, res, next) =>
     (async () => {
       const scheduleId = req.params.scheduleId;
       const schedule = await Schedule.findByPk(scheduleId, {
@@ -177,7 +183,7 @@ router.post(
           memo: req.body.memo.slice(0, 255) || 'noMemo',
           updatedAt,
         });
-        await fetchCreateCandidates(req, scheduleId);
+        await createCandidates(req, scheduleId);
         res.redirect(`/schedules/${scheduleId}`);
       };
       const fetchDelete = async () => {
@@ -191,8 +197,7 @@ router.post(
       } else {
         return next(createError(400, 'badRequest'));
       }
-    })().catch(next);
-  }
+    })().catch(next)
 );
 
 router.deleteScheduleAggregate = deleteScheduleAggregate;
