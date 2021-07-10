b873c4745194e7b4c8389fe6b124e166dc1f1d6d
suiran256 <suiran256@gmail.com>
2021-07-10 22:49:10 +0900

    availability切り替え不具合の修正

--- app/entry.js
+++ app/entry.js
@@ -5,30 +5,32 @@ import 'bootstrap';
 // const global = Function('return this;')();
 // global.jQuery = $;
 
-[$('.availability-toggle-button').get(0)].forEach((e) => {
-  // $('.availability-toggle-button').each((i, e) => {
-  const button = $(e);
-  button.click(() => {
-    const scheduleId = button.data('schedule-id');
-    const userId = button.data('user-id');
-    const candidateId = button.data('candidate-id');
-    const availability = parseInt(button.data('availability'), 10);
-    const nextAvailability = (availability + 1) % 3;
-    $.post(
-      `/schedules/${scheduleId}/users/${userId}/candidates/${candidateId}`,
-      { availability: nextAvailability },
-      (data) => {
-        button.data('availability', data.availability);
-        const availabilityLabels = ['ketsu', '?', 'de'];
-        button.text(availabilityLabels[data.availability]);
+$('.availability-toggle-button')
+  .get()
+  .forEach((e) => {
+    // $('.availability-toggle-button').each((i, e) => {
+    const button = $(e);
+    button.click(() => {
+      const scheduleId = button.data('schedule-id');
+      const userId = button.data('user-id');
+      const candidateId = button.data('candidate-id');
+      const availability = parseInt(button.data('availability'), 10);
+      const nextAvailability = (availability + 1) % 3;
+      $.post(
+        `/schedules/${scheduleId}/users/${userId}/candidates/${candidateId}`,
+        { availability: nextAvailability },
+        (data) => {
+          button.data('availability', data.availability);
+          const availabilityLabels = ['ketsu', '?', 'de'];
+          button.text(availabilityLabels[data.availability]);
 
-        const buttonStyles = ['btn-danger', 'btn-secondary', 'btn-success'];
-        button.removeClass('btn-danger btn-secondary btn-success');
-        button.addClass(buttonStyles[data.availability]);
-      }
-    );
+          const buttonStyles = ['btn-danger', 'btn-secondary', 'btn-success'];
+          button.removeClass('btn-danger btn-secondary btn-success');
+          button.addClass(buttonStyles[data.availability]);
+        }
+      );
+    });
   });
-});
 
 const buttonSelfComment = $('#self-comment-button');
 buttonSelfComment.click(() => {
